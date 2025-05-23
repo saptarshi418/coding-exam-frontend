de1b5eef8ef35be201bc30564c414abeefea8937
animesh-chakrabarty <animeshchakrabarty.dev@gmail.com>
2025-05-23 22:22:10 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-xl text-red-600'>Hello world</h1>
+      <button type="button" class="btn btn-primary">Primary</button>
+
+    </div>
+  )
+}
+
+export default App
